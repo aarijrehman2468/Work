8be8f51bb3b2fc6afbe861dc3d1c3eabdf388b81
aarijrehman2468 <aarijrehman2468@gmail.com>
2024-10-05 23:15:22 +0500

    features signup working

---
+++ App.js
@@ -0,0 +1,5 @@
+function buyNow() {
+    alert("Thank you for your interest! You will be redirected to the purchase page.");
+    // Here you can add a redirection if needed
+    // window.location.href = 'purchase-page-url.html';
+}
